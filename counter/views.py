902b14d1c415568c0f2a4a296f0f3dd1ce76d5cd
manjeeth <djmanjith@gmail.com>
2022-06-22 15:41:02 +0530

    Added files to repo
    This is the first commit to this repository

---
+++ counter/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    import json
+    import requests
+    if request.method == 'POST':
+        query = request.POST['query']
+        api_url = 'https://api.api-ninjas.com/v1/nutrition?query='
+        api_request = requests.get(
+            api_url + query, headers={'X-Api-Key': 'YOUR-API-KEY'})
+        try:
+            api = json.loads(api_request.content)
+            print(api_request.content)
+        except Exception as e:
+            api = "oops! There was an error"
+            print(e)
+        return render(request, 'home.html', {'api': api})
+    else:
+        return render(request, 'home.html', {'query': 'Enter a valid query'})
